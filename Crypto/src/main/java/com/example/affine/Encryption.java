6f1b157c095e9d1db52892ce48d64b369f336494
Omar Goda <80228400+omargoda99@users.noreply.github.com>
2025-02-18 23:09:21 +0200

    Update Encryption.java

--- Crypto/src/main/java/com/example/affine/Encryption.java
+++ Crypto/src/main/java/com/example/affine/Encryption.java
@@ -3,13 +3,7 @@ package com.example.affine;
 public class Encryption {
     private int a;
     private int b;
-    private String encrypted;
-
-    Encryption(int a, int b, String encrypted) {
-        this.a = a;
-        this.b = b;
-        this.encrypted = encrypted;
-    }
+    private String plain;
 
     public void setA(int a) {
         this.a = a;
@@ -19,57 +13,39 @@ public class Encryption {
         this.b = b;
     }
 
-    public void setEncrypted(String encrypted) {
-        this.encrypted = encrypted;
+    public void setPlain(String plain) {
+        this.plain = plain;
     }
 
-    public int getA() {
-        return a;
-    }
-
-    public int getB() {
-        return b;
-    }
-
-    public String getEncrypted() {
-        return encrypted;
-    }
-
-    public String encrypt(int a, int b, String encrypted) {
-        if (a < 0) {
-            a += 26;
-            a %= 26;
-        }
-        if (b < 0) {
-            b += 26;
-            b %= 26;
-        }
-        a %= 26;
-        b %= 26;
+    public String encrypt(int a, int b, String plain) {
+        if (a < 0) a = (a % 26 + 26) % 26;
+        if (b < 0) b = (b % 26 + 26) % 26;
         if (gcd(a, 26) != 1) {
-            return "GCD a and 26 is not 1 relax and try again with some focus :>";
+            return "GCD of a and 26 is not 1. Please use a valid key.";
         } else {
             StringBuilder stringBuilder = new StringBuilder();
-            for (int i = 0; i < encrypted.length(); i++) {
-                char ch = encrypted.charAt(i);
+            for (int i = 0; i < plain.length(); i++) {
+                char ch = plain.charAt(i);
                 if (Character.isUpperCase(ch)) {
-                    char enc = (char) (((a * (ch - 'A') + b) % 26) + 'A');
-                    stringBuilder.append(ch);
+                    char enc = (char) ((((a * (ch - 'A')) + b) % 26) + 'A');
+                    stringBuilder.append(enc);
                 } else if (Character.isLowerCase(ch)) {
-                    char enc = (char) (((a * (ch - 'a') + b) % 26) + 'a');
-                    stringBuilder.append(ch);
-                }
-                else{
+                    char enc = (char) ((((a * (ch - 'a')) + b) % 26) + 'a');
+                    stringBuilder.append(enc);
+                } else {
                     stringBuilder.append(ch);
                 }
             }
             return stringBuilder.toString();
         }
     }
-    public int gcd(int a, int b) {
-        if (b == 0) {
-            return a;
-        }
-        return gcd(b, a % b);
+
+    private int gcd(int a, int b) {
+        return (b == 0) ? a : gcd(b, a % b);
+    }
+
+    @Override
+    public String toString() {
+        return "The Original Text is " + plain + " and the encryption of a = " + a + " and b = " + b + " is " + encrypt(a, b, plain);
     }
 }
